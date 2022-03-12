b5f6db4054c54c2640fa1d910fc71eb6cdd5adf6
Joseph Kim <91978539+jkim0521@users.noreply.github.com>
2022-03-12 12:28:33 -0700

    added constructors

--- ToyStoreCompany/src/mru/tsc/view/Menu.java
+++ ToyStoreCompany/src/mru/tsc/view/Menu.java
@@ -1,4 +1,5 @@
 package mru.tsc.view;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -54,14 +55,14 @@ public class Menu {
 	
 	public String searchSN() {
 		String sn = "";
-		System.out.println("Enter SN: ");
+		System.out.println("Enter Serial Number: ");
 		sn = option.nextLine();
 		return sn;
 	}
 	
 	public String searchName() {
 		String name = "";
-		System.out.println("Enter Name: ");
+		System.out.println("Enter Toy Name: ");
 		name = option.nextLine();
 		return name;
 	}
@@ -71,6 +72,186 @@ public class Menu {
 		System.out.println("Enter Type: ");
 		type = option.nextLine();
 		return type;
+	}
+	
+	/**
+	 * Prompt the user to enter a valid serial number for adding a toy
+	 * @return
+	 */
+	public String enterSN() {
+		String sn = "";
+		System.out.println("Enter Serial Number: ");
+		sn = option.nextLine();
+		return sn;
+	}
+	
+	/**
+	 * Prompt the user to enter a valid name for the toy when adding a toy
+	 * @return
+	 */
+	public String enterName() {
+		String name = "";
+		System.out.println("Enter Toy Name: ");
+		name = option.nextLine();
+		return name;
+	}
+	
+	/**
+	 * Prompt the user to enter a valid name for the toy when adding a toy
+	 * @return
+	 */
+	public String enterBrand() {
+		String brand = "";
+		System.out.println("Enter Toy Brand: ");
+		brand = option.nextLine();
+		return brand;
+	}
+	
+	/**
+	 * Prompt the user to enter a valid price for the toy when adding a toy
+	 * The price is rounded to two decimal places before it is returned
+	 * @return
+	 */
+	public double enterPrice() {
+		double price;
+		System.out.println("Enter Toy Price: ");
+		price = option.nextDouble();
+		price = Math.round(price*100.0)/100.0;
+		return price;
+	}
+	
+	/**
+	 * Prompt the user to enter number of toys available for purchase in the
+	 * current inventory, this should decrement as toys are purchased
+	 * @return
+	 */
+	public int enterCount() {
+		int availableCount;
+		System.out.println("Enter Available Counts: ");
+		try {
+			availableCount = option.nextInt();
+			return availableCount;
+		}
+		catch (InputMismatchException imm) {
+			System.out.print("");
+		}
+		return -1;
+	}
+	
+	/**
+	 * Prompt the user to enter an age that is recommended for the toy
+	 * @return
+	 */
+	public int enterAge() {
+		int age;
+		System.out.println("Enter Appropriate Age: ");
+		try {
+			age = option.nextInt();
+			return age;
+		}
+		catch (InputMismatchException imm) {
+			System.out.print("");
+		}
+		return -1;
+	}
+	
+	/**
+	 * Prompt the user to enter a valid name for the toy when adding a toy
+	 * @return
+	 */
+	public char enterClassification() {
+		System.out.println("Enter a classification (A, D, H): ");
+		try {
+			char classification = option.next().charAt(0);
+			switch(classification) {
+			case 'a': //fall through
+			case 'A': classification = Character.toUpperCase(classification);
+					  return classification;
+			case 'd': //fall through
+			case 'D': classification = Character.toUpperCase(classification);
+					  return classification;
+			case 'h': //fall through
+			case 'H': classification = Character.toUpperCase(classification);
+					  return classification;
+			default: return 'Z';
+			}
+		}
+		catch (InputMismatchException imm) {
+			System.out.print("");
+		}
+		return 'Z';
+	}
+	
+	/**
+	 * Prompt the user to enter a valid material for a stuffed Animal when adding a toy
+	 * @return
+	 */
+	public String enterMaterial() {
+		String material = "";
+		System.out.println("Enter Material: ");
+		material = option.nextLine();
+		return material;
+	}
+	
+	/**
+	 * Prompt the user to enter a valid name for the toy when adding a toy
+	 * @return
+	 */
+	public char enterSize() {
+		System.out.println("Enter a size (S, M, L): ");
+		try {
+			char size = option.next().charAt(0);
+			switch(size) {
+			case 's': //fall through
+			case 'S': size = Character.toUpperCase(size);
+					  return size;
+			case 'm': //fall through
+			case 'M': size = Character.toUpperCase(size);
+					  return size;
+			case 'l': //fall through
+			case 'L': size = Character.toUpperCase(size);
+					  return size;
+			default: return 'Z';
+			}
+		}
+		catch (InputMismatchException imm) {
+			System.out.print("");
+		}
+		return 'Z';
+	}
+	
+	/**
+	 * Verifies the length of a given serial number to match the system's requirements
+	 * of 10 digits (9 digits following after the significant digit).
+	 * @param serialNumber
+	 * @return returns true if the serial number is an appropriate length
+	 */
+	public boolean verifySN(String serialNumber) {
+		String sn = serialNumber;
+		int length = sn.length();
+		if (length == 10) {
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Verifies the type of toy that is being recorded by examining the significant digit
+	 * of the given serial number (0-1 figures, 2-3 animals, 4-6 puzzles, 7-9 boardgames). 
+	 * @param serialNumber
+	 * @return the significant digit of the serial number
+	 */
+	public int verifyType(String serialNumber) {
+		String sn = serialNumber;
+		String digit = sn.substring(0,1);
+		try {
+			int sigdig = Integer.parseInt(digit);
+			return sigdig;
+		}
+		catch (NumberFormatException nfe) {
+			System.out.println("This is an invalid Serial Number! Please input only integers.");
+		}
+		return -1;
 	}
 	
 	/**
